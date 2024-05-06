c9c448e764b990fb286dc71af5ab7ef582c8065f
文斌 <>
2024-05-06 16:01:05 +0800

    英雄系统作业（50%）

--- pytest_homework/tests/test_addhero.py
+++ pytest_homework/tests/test_addhero.py
@@ -1,24 +1,13 @@
 import os.path
 
+import allure
 import pytest
 import yaml
 from pytest_homework.script.hero import HeroManagement
 from pytest_homework.utils.log_util import logger
 from pytest_homework.utils.utils import Utils
 
-
-def get_datas(name, level):
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    system = Utils.get_windos_or_mac()
-    if system == "Windows":
-        yaml_path = os.sep.join([root_path, '..', 'datas\create_hero_data.yaml'])
-    else:
-        yaml_path = os.sep.join([root_path, '..', 'datas/create_hero_data.yaml'])
-    yaml_datas = Utils.get_yaml_data(yaml_path)
-    datas = yaml_datas.get(name).get(level).get("datas")
-    ids = yaml_datas.get(name).get(level).get("ids")
-    return [datas, ids]
-
+@allure.feature("创建英雄功能")
 class Test_Add_Hero:
     def setup_class(self):
         self.heromanage = HeroManagement()
@@ -32,11 +21,14 @@ class Test_Add_Hero:
         logger.info("=======结束测试=======")
 
     @pytest.mark.parametrize(
-        "hero_name,hero_volume,hero_power,expected_results", get_datas('create', 'P0')[0], ids=get_datas('create', 'P0')[1]
+        "hero_name,hero_volume,hero_power,expected_results", Utils.get_datas('create', 'P0', "create_hero_data")[0], ids=Utils.get_datas('create', 'P0', "create_hero_data")[1]
     )
+    @allure.story("创建功能 P0 级别用例")
     def test_add_hero(self, hero_name, hero_volume, hero_power, expected_results):
         logger.info(f'创建英雄的名字为：{hero_name}，血量为：{hero_volume}，力量为：{hero_power}，预期结果为：{expected_results}')
-        create_hero_result = self.heromanage.create_hero(hero_name, hero_volume, hero_power)
-        logger.info(f'实际结果为：{create_hero_result}')
-        assert create_hero_result == expected_results
+        with allure.step("调用创建方法"):
+            create_hero_result = self.heromanage.create_hero(hero_name, hero_volume, hero_power)
+            logger.info(f'实际结果为：{create_hero_result}')
+        with allure.step(f"断言 {create_hero_result} == {expected_results}"):
+            assert create_hero_result == expected_results
 
--- pytest_homework/utils/utils.py
+++ pytest_homework/utils/utils.py
@@ -1,3 +1,4 @@
+import os
 import platform
 
 import yaml
@@ -26,3 +27,16 @@ class Utils:
             return 'Windows'
         else:
             return 'Unknown OS'
+
+    @classmethod
+    def get_datas(cls, name, level, yaml_name):
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        system = Utils.get_windos_or_mac()
+        if system == "Windows":
+            yaml_path = os.sep.join([root_path, '..', f'datas\\{yaml_name}.yaml'])
+        else:
+            yaml_path = os.sep.join([root_path, '..', f'datas/{yaml_name}.yaml'])
+        yaml_datas = Utils.get_yaml_data(yaml_path)
+        datas = yaml_datas.get(name).get(level).get("datas")
+        ids = yaml_datas.get(name).get(level).get("ids")
+        return [datas, ids]
